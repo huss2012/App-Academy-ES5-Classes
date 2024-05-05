91cd3ad59e13d9ea0d4d9554b0a0359438a89675
huss2012 <huss24940@gmail.com>
2024-05-05 21:24:45 +0300

    Phase 2: Monkey-patch the Array classis done.

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,18 @@
 // Your code here
+
+//1. Add isEqual function to the Array's Prototype:
+Array.prototype.isEqual = function (array) {
+    if (this.length !== array.length) {
+        return false;
+    }
+    for (let i = 0; i < array.length; i++){
+        if (this[i] !== array[i]) {
+            return false;
+        }
+    };
+    return true;
+}
+
+//--------------Local Test Area----------------
+console.log([1, 2, 3].isEqual([1, 2, 3]));//true
+console.log([1, 2, 3].isEqual([1, 2, 3, 2]));//false
